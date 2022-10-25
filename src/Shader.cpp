2ac5e671ea05f5e223ba26f9d0d3a21766834305
Baxter Cooper <baxtery.cooper@gmail.com>
2022-10-25 06:05:49 +0100

    moved shaders to /shaders and created Shader Class

---
+++ src/Shader.cpp
@@ -0,0 +1,31 @@
+#include <Shader.h>
+
+std::string get_file_contents(const char* filename) {
+	std::ifstream in(filename, std::ios::binary);
+	if (in) {
+		std::string contents;
+		in.seekg(0, std::ios::end);
+		contents.resize(in.tellg());
+		in.seekg(0, std::ios::beg);
+		in.read(&contents[0], contents.size());
+		in.close();
+		return(contents);
+	}
+
+	throw(errno);
+}
+
+Shader::Shader(const char* filename, int shaderType) {
+    ID = glCreateShader(shaderType);
+
+	// Read vertexFile and fragmentFile and store the strings
+	std::string shaderCode = get_file_contents(filename);
+
+	// Convert the shader source strings into character arrays
+	const char* shaderSource = shaderCode.c_str();
+
+	// Attach Vertex Shader source to the Vertex Shader Object
+	glShaderSource(ID, 1, &shaderSource, NULL);
+	// Compile the Vertex Shader into machine code
+	glCompileShader(ID);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <Shader.h>
 #include <config.h>
 
 const unsigned short OPENGL_MAJOR_VERSION = 4;
@@ -24,53 +25,6 @@ GLuint indices[] =
 	0, 2, 1,
 	0, 3, 2
 };
-
-
-const char* screenVertexShaderSource = R"(#version 460 core
-layout (location = 0) in vec3 pos;
-layout (location = 1) in vec2 uvs;
-out vec2 UVs;
-void main()
-{
-	gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
-	UVs = uvs;
-})";
-const char* screenFragmentShaderSource = R"(#version 460 core
-out vec4 FragColor;
-uniform sampler2D screen;
-in vec2 UVs;
-void main()
-{
-	FragColor = texture(screen, UVs);
-})";
-const char* screenComputeShaderSource = R"(#version 460 core
-layout(local_size_x = 8, local_size_y = 4, local_size_z = 1) in;
-layout(rgba32f, binding = 0) uniform image2D screen;
-void main()
-{
-	vec4 pixel = vec4(0.075, 0.133, 0.173, 1.0);
-	ivec2 pixel_coords = ivec2(gl_GlobalInvocationID.xy);
-	
-	ivec2 dims = imageSize(screen);
-	float x = -(float(pixel_coords.x * 2 - dims.x) / dims.x); // transforms to [-1.0, 1.0]
-	float y = -(float(pixel_coords.y * 2 - dims.y) / dims.y); // transforms to [-1.0, 1.0]
-	float fov = 90.0;
-	vec3 cam_o = vec3(0.0, 0.0, -tan(fov / 2.0));
-	vec3 ray_o = vec3(x, y, 0.0);
-	vec3 ray_d = normalize(ray_o - cam_o);
-	vec3 sphere_c = vec3(0.0, 0.0, -5.0);
-	float sphere_r = 1.0;
-	vec3 o_c = ray_o - sphere_c;
-	float b = dot(ray_d, o_c);
-	float c = dot(o_c, o_c) - sphere_r * sphere_r;
-	float intersectionState = b * b - c;
-	vec3 intersection = ray_o + ray_d * (-b + sqrt(b * b - c));
-	if (intersectionState >= 0.0)
-	{
-		pixel = vec4((normalize(intersection - sphere_c) + 1.0) / 2.0, 1.0);
-	}
-	imageStore(screen, pixel_coords, pixel);
-})";
 
 int main() {
 	glfwInit();
@@ -124,30 +78,24 @@ int main() {
 	glTextureStorage2D(screenTex, 1, GL_RGBA32F, WINDOW_WIDTH, WINDOW_HEIGHT);
 	glBindImageTexture(0, screenTex, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
 
-	GLuint screenVertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(screenVertexShader, 1, &screenVertexShaderSource, NULL);
-	glCompileShader(screenVertexShader);
-	GLuint screenFragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(screenFragmentShader, 1, &screenFragmentShaderSource, NULL);
-	glCompileShader(screenFragmentShader);
+	Shader vertexShader("./shaders/default.vert", GL_VERTEX_SHADER);
+	Shader fragmentShader("./shaders/default.frag", GL_FRAGMENT_SHADER);
 
-	GLuint screenShaderProgram = glCreateProgram();
-	glAttachShader(screenShaderProgram, screenVertexShader);
-	glAttachShader(screenShaderProgram, screenFragmentShader);
-	glLinkProgram(screenShaderProgram);
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader.ID);
+	glAttachShader(shaderProgram, fragmentShader.ID);
+	glLinkProgram(shaderProgram);
 
-	glDeleteShader(screenVertexShader);
-	glDeleteShader(screenFragmentShader);
+	glDeleteShader(vertexShader.ID);
+	glDeleteShader(fragmentShader.ID);
 
-
-	GLuint computeShader = glCreateShader(GL_COMPUTE_SHADER);
-	glShaderSource(computeShader, 1, &screenComputeShaderSource, NULL);
-	glCompileShader(computeShader);
+	Shader computeShader("./shaders/default.comp", GL_COMPUTE_SHADER);
 
 	GLuint computeProgram = glCreateProgram();
-	glAttachShader(computeProgram, computeShader);
+	glAttachShader(computeProgram, computeShader.ID);
 	glLinkProgram(computeProgram);
 
+	glDeleteShader(computeShader.ID);
 
 	int work_grp_cnt[3];
 	glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 0, &work_grp_cnt[0]);
@@ -177,9 +125,9 @@ int main() {
 		glDispatchCompute(ceil(WINDOW_WIDTH / 8), ceil(WINDOW_HEIGHT / 4), 1);
 		glMemoryBarrier(GL_ALL_BARRIER_BITS);
 
-		glUseProgram(screenShaderProgram);
+		glUseProgram(shaderProgram);
 		glBindTextureUnit(0, screenTex);
-		glUniform1i(glGetUniformLocation(screenShaderProgram, "screen"), 0);
+		glUniform1i(glGetUniformLocation(shaderProgram, "screen"), 0);
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(indices[0]), GL_UNSIGNED_INT, 0);
 
