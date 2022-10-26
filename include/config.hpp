89f700b32e29fe2bf6427967a3ea8a36c0bdd3b2
cookies-xor-cream <aryageza@gmail.com>
2022-10-26 17:32:27 +0800

    Parameter tuning

--- include/config.hpp
+++ include/config.hpp
@@ -6,8 +6,8 @@ const unsigned short WINDOW_WIDTH = 800;
 const unsigned short WINDOW_HEIGHT = 600;
 
 /* AGENT */
-const unsigned int AGENT_COUNT = 100;
-const float AGENT_SPEED = 0.0001f;
+const unsigned int AGENT_COUNT = 1024;
+const float AGENT_SPEED = 0.00001f;
 
 /* AGENT - SENSOR */
 const unsigned short SENSOR_SIZE = 1;
