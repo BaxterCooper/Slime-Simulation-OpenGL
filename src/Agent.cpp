23e122364cbb52c5d5c0df732dcb90d5676715de
Baxter Cooper <baxtery.cooper@gmail.com>
2022-10-27 12:54:51 +1100

    testing with vertex shader

--- src/Agent.cpp
+++ src/Agent.cpp
@@ -1,6 +1,6 @@
 #include <Agent.hpp>
 
 Agent::Agent() {
-    position = glm::vec2(glm::linearRand(0.0, 1.0), glm::linearRand(0.0, 1.0));
+    position = glm::vec2(glm::linearRand(-1.0, 1.0), glm::linearRand(-1.0, 1.0));
     direction = glm::normalize(glm::vec2(glm::linearRand(-1.0, 1.0), glm::linearRand(-1.0, 1.0)));
 };
--- src/main.cpp
+++ src/main.cpp
@@ -20,26 +20,21 @@ const unsigned short OPENGL_MINOR_VERSION = 6;
 
 const bool vSync = true;
 
-GLfloat vertices[] =
-{
-	-1.0f, -1.0f , 0.0f, 0.0f, 0.0f,
-	-1.0f,  1.0f , 0.0f, 0.0f, 1.0f,
-	 1.0f,  1.0f , 0.0f, 1.0f, 1.0f,
-	 1.0f, -1.0f , 0.0f, 1.0f, 0.0f,
-};
-
-GLuint indices[] =
-{
-	0, 2, 1,
-	0, 3, 2
-};
-
 Agent *createAgents() {
 	Agent *agents = new Agent[AGENT_COUNT];
 	for (int i = 0; i < AGENT_COUNT; i++) {
 		agents[i] = Agent();
 	}
 	return agents;
+}
+
+GLfloat *getAgentVertices(Agent *agents) {
+	GLfloat *vertices = new GLfloat[AGENT_COUNT * 2];
+	for (int i = 0; i < AGENT_COUNT; i++) {
+		vertices[i*2] = agents[i].position.x;
+		vertices[i*2 + 1] = agents[i].position.y;
+	}
+	return vertices;
 }
 
 int main() {
@@ -67,64 +62,8 @@ int main() {
 
 	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
-	// ---------------- BEGIN SHADER DATA ----------------
-
-	// We generate a bunch of positions
-	Agent *agents = createAgents();
-    std::vector<glm::vec4> agentData(AGENT_COUNT);
-
-	// initial position
-    for(int i = 0; i < AGENT_COUNT; i++) {
-        glm::vec2 position = agents[i].position;
-        glm::vec2 direction = agents[i].direction;
-		agentData[i] = glm::vec4(position.x, position.y, direction.x, direction.y);
-    }
-
-	GLuint agentDataBuffer;
-	glCreateBuffers(1, &agentDataBuffer);
-
-    // fill with initial data
-    glBindBuffer(GL_ARRAY_BUFFER, agentDataBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec4)*AGENT_COUNT, &agentData[0], GL_STATIC_DRAW);
-
-    // set up generic attrib pointers: do I need these?
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4*sizeof(GLfloat), (char*)0 + 0*sizeof(GLfloat));
-
-	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, agentDataBuffer);
-
-	// ---------------- END SHADER DATA ----------------
-
-	GLuint VAO, VBO, EBO;
-	glCreateVertexArrays(1, &VAO);
-	glCreateBuffers(1, &VBO);
-	glCreateBuffers(1, &EBO);
-
-	glNamedBufferData(VBO, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glNamedBufferData(EBO, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	glEnableVertexArrayAttrib(VAO, 0);
-	glVertexArrayAttribBinding(VAO, 0, 0);
-	glVertexArrayAttribFormat(VAO, 0, 3, GL_FLOAT, GL_FALSE, 0);
-
-	glEnableVertexArrayAttrib(VAO, 1);
-	glVertexArrayAttribBinding(VAO, 1, 0);
-	glVertexArrayAttribFormat(VAO, 1, 2, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat));
-
-	glVertexArrayVertexBuffer(VAO, 0, VBO, 0, 5 * sizeof(GLfloat));
-	glVertexArrayElementBuffer(VAO, EBO);
-
-	GLuint screenTex;
-	glCreateTextures(GL_TEXTURE_2D, 1, &screenTex);
-	glTextureParameteri(screenTex, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTextureParameteri(screenTex, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTextureParameteri(screenTex, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTextureParameteri(screenTex, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTextureStorage2D(screenTex, 1, GL_RGBA32F, WINDOW_WIDTH, WINDOW_HEIGHT);
-	glBindImageTexture(0, screenTex, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
-
-	Shader vertexShader("./shaders/default.vert", GL_VERTEX_SHADER);
-	Shader fragmentShader("./shaders/default.frag", GL_FRAGMENT_SHADER);
+	Shader vertexShader("./shaders/agent.vert", GL_VERTEX_SHADER);
+	Shader fragmentShader("./shaders/agent.frag", GL_FRAGMENT_SHADER);
 
 	GLuint shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader.ID);
@@ -134,30 +73,66 @@ int main() {
 	glDeleteShader(vertexShader.ID);
 	glDeleteShader(fragmentShader.ID);
 
-	Shader computeShader("./shaders/blur.comp", GL_COMPUTE_SHADER);
+	Agent *agents = createAgents();
 
-	GLuint computeProgram = glCreateProgram();
-	glAttachShader(computeProgram, computeShader.ID);
-	glLinkProgram(computeProgram);
+	// Vertices coordinates
+	GLfloat *vertices = getAgentVertices(agents);
 
-	glDeleteShader(computeShader.ID);
+	// Create reference containers for the Vartex Array Object and the Vertex Buffer Object
+	glEnable(GL_PROGRAM_POINT_SIZE);
+	GLuint VAO, VBO;
 
-	while (!glfwWindowShouldClose(window)) {
-		glUseProgram(computeProgram);
-		glUniform1f(glGetUniformLocation(computeProgram, "agentSpeed"), AGENT_SPEED);
-		glDispatchCompute(ceil(WINDOW_WIDTH / 8), ceil(WINDOW_HEIGHT / 4), 1);
-		glMemoryBarrier(GL_ALL_BARRIER_BITS);
+	// Generate the VAO and VBO with only 1 object each
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
 
+	// Make the VAO the current Vertex Array Object by binding it
+	glBindVertexArray(VAO);
+
+	// Bind the VBO specifying it's a GL_ARRAY_BUFFER
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// Introduce the vertices into the VBO
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 2 * AGENT_COUNT, vertices, GL_STATIC_DRAW);
+
+	// Configure the Vertex Attribute so that OpenGL knows how to read the VBO
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	// Enable the Vertex Attribute so that OpenGL knows to use it
+	glEnableVertexAttribArray(0);
+
+	// Bind both the VBO and VAO to 0 so that we don't accidentally modify the VAO and VBO we created
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+
+
+	// Main while loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Specify the color of the background
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		// Clean the back buffer and assign the new color to it
+		glClear(GL_COLOR_BUFFER_BIT);
+		// Tell OpenGL which Shader Program we want to use
 		glUseProgram(shaderProgram);
-		glBindTextureUnit(0, screenTex);
-		glUniform1i(glGetUniformLocation(shaderProgram, "screen"), 0);
+		// Bind the VAO so OpenGL knows to use it
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(indices[0]), GL_UNSIGNED_INT, 0);
-
+		// Draw the triangle using the GL_TRIANGLES primitive
+		glDrawArrays(GL_POINTS, 0, AGENT_COUNT);
+		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
+		// Take care of all GLFW events
 		glfwPollEvents();
 	}
 
+
+
+	// Delete all the objects we've created
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+	// Delete window before ending the program
 	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
 	glfwTerminate();
+	return 0;
 }
