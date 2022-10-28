7311543067b5a4caef083958ab6d33e92cf7aa3a
cookies-xor-cream <aryageza@gmail.com>
2022-10-28 19:56:06 +0800

    Fixed texture lookup logic, tuned config

--- include/config.hpp
+++ include/config.hpp
@@ -6,14 +6,14 @@ const unsigned short WINDOW_WIDTH = 800;
 const unsigned short WINDOW_HEIGHT = 600;
 
 /* AGENT */
-const unsigned int AGENT_COUNT = 1024;
-const float AGENT_MOVE_SPEED = 0.00001f;
+const unsigned int AGENT_COUNT = 256;
+const float AGENT_MOVE_SPEED = 0.00006f;
 const float AGENT_TURN_SPEED = 1.0f;
 
 /* AGENT - SENSOR */
 const unsigned short SENSOR_SIZE = 3;
-const float SENSOR_OFFSET = 0.001f;
-const float SENSOR_FOV = 70.0f;
+const float SENSOR_OFFSET = 0.01f;
+const float SENSOR_FOV = 0.8f;
 
 /* SHADERS */
 const float BLUR_SPEED = 0.8f;
