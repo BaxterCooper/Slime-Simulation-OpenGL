4ecb8351d0323fa75734accade03e393fe89b026
Baxter Cooper <baxtery.cooper@gmail.com>
2022-10-28 12:52:29 +0800

    fixed agent direction generation

--- src/Agent.cpp
+++ src/Agent.cpp
@@ -2,5 +2,5 @@
 
 Agent::Agent() {
     position = glm::vec2(glm::linearRand(-1.0, 1.0), glm::linearRand(-1.0, 1.0));
-    direction = glm::normalize(glm::vec2(glm::linearRand(0.5, 1.0), glm::linearRand(0.5, 1.0)));
+    direction = glm::normalize(glm::vec2(glm::linearRand(-1.0, 1.0), glm::linearRand(-1.0, 1.0)));
 };
