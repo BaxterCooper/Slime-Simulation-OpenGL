13e4cd4ec80c3992a2584c3978d6010b1f9b62b2
Baxter Cooper <baxtery.cooper@gmail.com>
2022-10-31 16:27:54 +0800

    random bouncing

--- include/config.hpp
+++ include/config.hpp
@@ -2,21 +2,21 @@
 #define CONFIG
 
 /* WINDOW */
-const unsigned short WINDOW_WIDTH = 800;
-const unsigned short WINDOW_HEIGHT = 600;
+const unsigned short WINDOW_WIDTH = 600;
+const unsigned short WINDOW_HEIGHT = 400;
 
 /* AGENT */
 const unsigned short AGENT_COUNT = 1024;
-const float AGENT_MOVE_SPEED = 0.00002f; // As a proportion of the screen per frame
+const float AGENT_MOVE_SPEED = 0.005f;
 const float AGENT_TURN_SPEED = 0.01f;
 
 /* AGENT - SENSOR */
-const unsigned short SENSOR_SIZE = 1; // In pixels
-const float SENSOR_OFFSET = 0.1f; // As a proportion of the screen
-const float SENSOR_FOV = 0.3f; // IN RADIANS, keep small
+const unsigned short SENSOR_SIZE = 1;
+const float SENSOR_OFFSET = 0.3f;
+const float SENSOR_FOV = 0.3f;
 
 /* SHADERS */
 const float BLUR_SPEED = 0.8f;
-const float FADE_SPEED = 0.002f; // linear fade
+const float FADE_SPEED = 0.001f;
 
 #endif
