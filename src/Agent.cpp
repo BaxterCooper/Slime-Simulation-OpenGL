62518901ad85205f72475e2e174342de5f5e7d4b
cookies-xor-cream <aryageza@gmail.com>
2022-10-31 20:11:57 +0800

    Agents now spawn in a circle facing in

--- src/Agent.cpp
+++ src/Agent.cpp
@@ -1,6 +1,11 @@
 #include <Agent.hpp>
+#include "./config.hpp"
 
 Agent::Agent() {
-    position = glm::vec2(glm::linearRand(-1.0, 1.0), glm::linearRand(-1.0, 1.0));
-    direction = glm::normalize(glm::vec2(glm::linearRand(-1.0, 1.0), glm::linearRand(-1.0, 1.0)));
+    float theta = glm::linearRand(0.0, 3.14 * 2.0);
+    float radius = 0.99;
+    float magnitude = glm::linearRand(0.0, 1.0);
+    float aspectRatio = float(WINDOW_WIDTH) / float(WINDOW_HEIGHT);
+    position = glm::vec2(sin(theta) * radius * magnitude, cos(theta) * radius * magnitude * aspectRatio);
+    direction = -glm::normalize(position);
 };
