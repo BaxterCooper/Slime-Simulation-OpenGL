fe09b66bebd096136bd714b498434f23204e539e
Baxter Cooper <baxtery.cooper@gmail.com>
2022-11-01 13:04:46 +0800

    optimisations

--- include/config.hpp
+++ include/config.hpp
@@ -2,13 +2,13 @@
 #define CONFIG
 
 /* WINDOW */
-const unsigned short WINDOW_WIDTH = 600;
-const unsigned short WINDOW_HEIGHT = 400;
+const unsigned short WINDOW_WIDTH = 800;
+const unsigned short WINDOW_HEIGHT = 600;
 
 /* AGENT */
-const unsigned int AGENT_COUNT = 1024;
-const float AGENT_MOVE_SPEED = 0.00001f; // As a proportion of the screen per frame
-const float AGENT_LUMINANCE = 0.5f;
+const unsigned int AGENT_COUNT = 16384;
+const float AGENT_MOVE_SPEED = 0.001f; // As a proportion of the screen per frame
+const float AGENT_LUMINANCE = 0.3f;
 const float AGENT_TURN_SPEED = 1.0f; // unimplemented
 
 /* AGENT - SENSOR */
--- src/main.cpp
+++ src/main.cpp
@@ -197,6 +197,7 @@ int main() {
 	glVertexArrayVertexBuffer(screenVAO, 0, screenVBO, 0, 2 * sizeof(GLfloat));
 	glVertexArrayElementBuffer(screenVAO, screenEBO);
 
+
 	// ------------------------------------------------------------------------
 	// MAIN WHILE LOOP
 	
@@ -234,7 +235,7 @@ int main() {
 		glUniform1f(glGetUniformLocation(agentComputeProgram, "sensorOffset"), SENSOR_OFFSET);
 		glUniform1f(glGetUniformLocation(agentComputeProgram, "sensorFOV"), SENSOR_FOV);
 		glUniform1i(glGetUniformLocation(agentComputeProgram, "time"), time(NULL));
-		glDispatchCompute(AGENT_COUNT, AGENT_COUNT, 1); // can be optimised
+		glDispatchCompute(AGENT_COUNT / 8, AGENT_COUNT / 8, 1); // can be optimised
 		glMemoryBarrier(GL_ALL_BARRIER_BITS); // can be optimised
 
 		glfwSwapBuffers(window);
